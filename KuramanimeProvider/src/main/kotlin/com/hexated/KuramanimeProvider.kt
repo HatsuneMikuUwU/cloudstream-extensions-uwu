ac6a8e414c3345e922242a73c893c8a37cf4eb6a
Hatsune Miku <146099520+HatsuneMikuUwU@users.noreply.github.com>
2025-10-21 04:08:12 +0700

    Try to fix

--- KuramanimeProvider/src/main/kotlin/com/hexated/KuramanimeProvider.kt
+++ KuramanimeProvider/src/main/kotlin/com/hexated/KuramanimeProvider.kt
@@ -3,11 +3,10 @@ package com.hexated
 import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.LoadResponse.Companion.addAniListId
 import com.lagradost.cloudstream3.LoadResponse.Companion.addMalId
-import com.lagradost.cloudstream3.utils.ExtractorLink
-import com.lagradost.cloudstream3.utils.INFER_TYPE
-import com.lagradost.cloudstream3.utils.Qualities
-import com.lagradost.cloudstream3.utils.loadExtractor
-import com.lagradost.cloudstream3.utils.newExtractorLink
+import com.lagradost.cloudstream3.utils.*
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Element
 
@@ -50,15 +49,11 @@ class KuramanimeProvider : MainAPI() {
         "$mainUrl/anime/movie?order_by=updated&page=" to "Film Layar Lebar",
     )
 
-    override suspend fun getMainPage(
-        page: Int,
-        request: MainPageRequest
-    ): HomePageResponse {
+    override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {
         val document = app.get(request.data + page).document
         val home = document.select("div#animeList div.product__item").mapNotNull {
             it.toSearchResult()
         }
-
         return newHomePageResponse(request.name, home)
     }
 
@@ -82,7 +77,6 @@ class KuramanimeProvider : MainAPI() {
             this.posterUrl = posterUrl
             addSub(episode)
         }
-
     }
 
     override suspend fun search(query: String): List<SearchResponse> {
@@ -98,9 +92,9 @@ class KuramanimeProvider : MainAPI() {
 
         val title = document.selectFirst(".anime__details__title > h3")!!.text().trim()
         val poster = document.selectFirst(".anime__details__pic")?.attr("data-setbg")
-        val tags =
-            document.select("div.anime__details__widget > div > div:nth-child(2) > ul > li:nth-child(1)")
-                .text().trim().replace("Genre: ", "").split(", ")
+        val tags = document
+            .select("div.anime__details__widget > div > div:nth-child(2) > ul > li:nth-child(1)")
+            .text().trim().replace("Genre: ", "").split(", ")
 
         val year = Regex("\\D").replace(
             document.select("div.anime__details__widget > div > div:nth-child(1) > ul > li:nth-child(5)")
@@ -113,7 +107,6 @@ class KuramanimeProvider : MainAPI() {
         val description = document.select(".anime__details__text > p").text().trim()
 
         val episodes = mutableListOf<Episode>()
-
         for (i in 1..30) {
             val doc = app.get("$url?page=$i").document
             val eps = Jsoup.parse(doc.select("#episodeLists").attr("data-content"))
@@ -132,6 +125,7 @@ class KuramanimeProvider : MainAPI() {
             document.selectFirst("div.col-lg-6.col-md-6 ul li:contains(Tipe:) a")?.text()
                 ?.lowercase() ?: "tv", episodes.size
         )
+
         val recommendations = document.select("div#randomList > a").mapNotNull {
             val epHref = it.attr("href")
             val epTitle = it.select("h5.sidebar-title-h5.px-2.py-2").text()
@@ -157,7 +151,6 @@ class KuramanimeProvider : MainAPI() {
             addMalId(tracker?.malId)
             addAniListId(tracker?.aniId?.toIntOrNull())
         }
-
     }
 
     private suspend fun invokeLocalSource(
@@ -166,13 +159,9 @@ class KuramanimeProvider : MainAPI() {
         headers: Map<String, String>,
         subtitleCallback: (SubtitleFile) -> Unit,
         callback: (ExtractorLink) -> Unit
-    ) {
-        val document = app.get(
-            url,
-            headers = headers,
-            cookies = cookies
-        ).document
-        document.select("video#player > source").map {
+    ) = coroutineScope {
+        val document = app.get(url, headers = headers, cookies = cookies).document
+        document.select("video#player > source").forEach {
             val link = fixUrl(it.attr("src"))
             val quality = it.attr("size").toIntOrNull()
             callback.invoke(
@@ -192,10 +181,14 @@ class KuramanimeProvider : MainAPI() {
                 }
             )
         }
+
         if (server == "kuramadrive") {
-            document.select("div#animeDownloadLink a").apmap {
-                loadExtractor(it.attr("href"), "$mainUrl/", subtitleCallback, callback)
-            }
+            // FIXED: replace apmap with async + awaitAll
+            document.select("div#animeDownloadLink a").map { link ->
+                async {
+                    loadExtractor(link.attr("href"), "$mainUrl/", subtitleCallback, callback)
+                }
+            }.awaitAll()
         }
     }
 
@@ -204,16 +197,14 @@ class KuramanimeProvider : MainAPI() {
         isCasting: Boolean,
         subtitleCallback: (SubtitleFile) -> Unit,
         callback: (ExtractorLink) -> Unit
-    ): Boolean {
+    ): Boolean = coroutineScope {
 
         val req = app.get(data)
         val res = req.document
         cookies = req.cookies
 
-        val token = res.selectFirst("meta[name=csrf-token]")?.attr("content") ?: return false
-        val dataKps =
-            res.selectFirst("div.col-lg-12.mt-3")?.attributes()?.last()?.value ?: return false
-
+        val token = res.selectFirst("meta[name=csrf-token]")?.attr("content") ?: return@coroutineScope false
+        val dataKps = res.selectFirst("div.col-lg-12.mt-3")?.attributes()?.last()?.value ?: return@coroutineScope false
         val assets = getAssets(dataKps)
 
         var headers = mapOf(
@@ -235,26 +226,26 @@ class KuramanimeProvider : MainAPI() {
             "X-Requested-With" to "XMLHttpRequest",
         )
 
-        res.select("select#changeServer option").apmap { source ->
-            val server = source.attr("value")
-            val link =
-                "$data?${assets.MIX_PAGE_TOKEN_KEY}=$tokenKey&${assets.MIX_STREAM_SERVER_KEY}=$server"
-            if (server.contains(Regex("(?i)kuramadrive|archive"))) {
-                invokeLocalSource(link, server, headers, subtitleCallback, callback)
-            } else {
-                app.get(
-                    link,
-                    referer = data,
-                    headers = headers,
-                    cookies = cookies
-                ).document.select("div.iframe-container iframe").attr("src").let { videoUrl ->
-                    loadExtractor(fixUrl(videoUrl), "$mainUrl/", subtitleCallback, callback)
+        // FIXED: replace apmap with async + awaitAll
+        res.select("select#changeServer option").map { source ->
+            async {
+                val server = source.attr("value")
+                val link =
+                    "$data?${assets.MIX_PAGE_TOKEN_KEY}=$tokenKey&${assets.MIX_STREAM_SERVER_KEY}=$server"
+
+                if (server.contains(Regex("(?i)kuramadrive|archive"))) {
+                    invokeLocalSource(link, server, headers, subtitleCallback, callback)
+                } else {
+                    app.get(link, referer = data, headers = headers, cookies = cookies)
+                        .document.select("div.iframe-container iframe")
+                        .attr("src").let { videoUrl ->
+                            loadExtractor(fixUrl(videoUrl), "$mainUrl/", subtitleCallback, callback)
+                        }
                 }
             }
-        }
+        }.awaitAll()
 
-
-        return true
+        true
     }
 
     private suspend fun getAssets(bpjs: String?): Assets {
@@ -280,9 +271,7 @@ class KuramanimeProvider : MainAPI() {
 
     private fun randomId(length: Int = 6): String {
         val allowedChars = ('a'..'z') + ('A'..'Z') + ('0'..'9')
-        return (1..length)
-            .map { allowedChars.random() }
-            .joinToString("")
+        return (1..length).map { allowedChars.random() }.joinToString("")
     }
 
     data class Assets(
@@ -293,5 +282,4 @@ class KuramanimeProvider : MainAPI() {
         val MIX_PAGE_TOKEN_KEY: String?,
         val MIX_STREAM_SERVER_KEY: String?,
     )
-
 }
