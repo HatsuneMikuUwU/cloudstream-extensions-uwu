bf9beba59635f529621d8efbec5a8f24855447ce
HatsuneMikuUwU <146099520+HatsuneMikuUwU@users.noreply.github.com>
2025-12-24 03:38:59 +0700

    Last Sync: 2025-12-24 03:38 (Mobile)

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,15 +5,16 @@ buildscript {
     repositories {
         google()
         mavenCentral()
-        // Shitpack repo which contains our tools and dependencies
+        // Repository JitPack untuk tools dan dependensi
         maven("https://jitpack.io")
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:8.7.3")
-        // Cloudstream gradle plugin which makes everything work and builds plugins
-        classpath("com.github.recloudstream:gradle:-SNAPSHOT")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
+        classpath("com.android.tools.build:gradle:8.2.2") // Versi AGP yang stabil
+        // FIX: Tambahkan 'master-' sebelum SNAPSHOT
+        classpath("com.github.recloudstream:gradle:master-SNAPSHOT")
+        // Disarankan Kotlin 1.9.23 agar kompatibel dengan Cloudstream saat ini
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.23")
     }
 }
 
@@ -35,9 +36,8 @@ subprojects {
     apply(plugin = "com.lagradost.cloudstream3.gradle")
 
     cloudstream {
-        // when running through github workflow, GITHUB_REPOSITORY should contain current repository name
+        // Jika berjalan di GitHub workflow, gunakan variabel environment repository
         setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/hexated/cloudstream-extensions-hexated")
-
         authors = listOf("Hexated")
     }
 
@@ -46,7 +46,8 @@ subprojects {
 
         defaultConfig {
             minSdk = 21
-            compileSdkVersion(35)
+            // FIX: Gunakan sintaks property, bukan method
+            compileSdk = 35 
             targetSdk = 35
         }
 
@@ -57,8 +58,7 @@ subprojects {
 
         tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
             kotlinOptions {
-                jvmTarget = "1.8" // Required
-                // Disables some unnecessary features
+                jvmTarget = "1.8"
                 freeCompilerArgs = freeCompilerArgs +
                         "-Xno-call-assertions" +
                         "-Xno-param-assertions" +
@@ -68,27 +68,21 @@ subprojects {
     }
 
     dependencies {
-        val apk by configurations
-        val implementation by configurations
+        // FIX: Gunakan 'compileOnly' atau 'cloudstream' configuration secara eksplisit
+        // untuk menghindari error "Configuration not found"
+        add("cloudstream", "com.lagradost:cloudstream3:pre-release")
 
-        // Stubs for all Cloudstream classes
-        apk("com.lagradost:cloudstream3:pre-release")
-
-        // these dependencies can include any of those which are added by the app,
-        // but you dont need to include any of them if you dont need them
-        // https://github.com/recloudstream/cloudstream/blob/master/app/build.gradle
-
-        implementation(kotlin("stdlib")) // adds standard kotlin features, like listOf, mapOf etc
-        implementation("com.github.Blatzar:NiceHttp:0.4.13") // http library
-        implementation("org.jsoup:jsoup:1.18.3") // html parser
+        implementation(kotlin("stdlib")) 
+        implementation("com.github.Blatzar:NiceHttp:0.4.13")
+        implementation("org.jsoup:jsoup:1.18.3")
         implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.16.1")
         implementation("io.karn:khttp-android:0.1.2")
         implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.0")
-        implementation("org.mozilla:rhino:1.7.14") //run JS
-
+        implementation("org.mozilla:rhino:1.7.14") 
     }
 }
 
-task<Delete>("clean") {
+// Task clean standar
+tasks.register<Delete>("clean") {
     delete(rootProject.buildDir)
 }
