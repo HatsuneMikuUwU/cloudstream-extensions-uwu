b09b08957190a6da9c86c4fc16cdf3de7fba693c
HatsuneMikuUwU <146099520+HatsuneMikuUwU@users.noreply.github.com>
2026-02-24 04:09:00 +0700

    Last Sync: 2026-02-24 04:08 (Mobile)

---
+++ DutamovieProvider/build.gradle.kts
@@ -0,0 +1,14 @@
+version = 1
+
+cloudstream {
+    description = "DutaMovie - Streaming Movie and TV Series"
+    language = "id"
+    authors = listOf("Miku")
+    status = 1
+    isCrossPlatform = true
+    tvTypes = listOf(
+        "Movie",
+        "TvSeries",
+        "AsianDrama",
+    )
+}
