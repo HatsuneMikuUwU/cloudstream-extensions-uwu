0eefdad9f82f4d86fee087e54e6c8e577836658f
HatsuneMikuUwU <146099520+HatsuneMikuUwU@users.noreply.github.com>
2026-02-25 05:47:16 +0700

    Last Sync: 2026-02-25 05:47 (Mobile)

--- DonghubProvider/src/main/kotlin/com/donghub/DonghubProviderPlugin.kt
+++ DonghubProvider/src/main/kotlin/com/donghub/DonghubProviderPlugin.kt
@@ -7,7 +7,6 @@ import com.lagradost.cloudstream3.plugins.Plugin
 @CloudstreamPlugin
 class DonghubProviderPlugin: Plugin() {
     override fun load(context: Context) {
-        Donghub.context = context
         registerMainAPI(DonghubProvider())
         registerExtractorAPI(ArchiveOrgExtractor())
         registerExtractorAPI(Dailymotion())
