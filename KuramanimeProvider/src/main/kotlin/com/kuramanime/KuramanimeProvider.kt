bfae93af1187c960bf6a1b085cc0eaeb9753b88c
HatsuneMikuUwU <146099520+HatsuneMikuUwU@users.noreply.github.com>
2026-03-07 01:40:45 +0700

    test

--- KuramanimeProvider/src/main/kotlin/com/kuramanime/KuramanimeProvider.kt
+++ KuramanimeProvider/src/main/kotlin/com/kuramanime/KuramanimeProvider.kt
@@ -4,14 +4,16 @@ import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.LoadResponse.Companion.addAniListId
 import com.lagradost.cloudstream3.LoadResponse.Companion.addMalId
 import com.lagradost.cloudstream3.amap
+import com.lagradost.cloudstream3.mvvm.safeApiCall
 import com.lagradost.cloudstream3.utils.ExtractorLink
+import com.lagradost.cloudstream3.utils.ExtractorLinkType
 import com.lagradost.cloudstream3.utils.INFER_TYPE
 import com.lagradost.cloudstream3.utils.Qualities
 import com.lagradost.cloudstream3.utils.loadExtractor
 import com.lagradost.cloudstream3.utils.newExtractorLink
+import kotlinx.coroutines.runBlocking
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Element
-import java.net.URI
 
 class KuramanimeProvider : MainAPI() {
     override var mainUrl = "https://v17.kuramanime.ink"
@@ -103,7 +105,7 @@ class KuramanimeProvider : MainAPI() {
         val yearText = document.select("div.anime__details__widget > div > div:nth-child(1) > ul > li:nth-child(5)")
             .text().trim().replace("Musim: ", "")
         val year = Regex("\\d+").find(yearText)?.value?.toIntOrNull()
-        
+
         val status = getStatus(
             document.select("div.anime__details__widget > div > div:nth-child(1) > ul > li:nth-child(3)")
                 .text().trim().replace("Status: ", "")
@@ -116,7 +118,7 @@ class KuramanimeProvider : MainAPI() {
             val doc = app.get("$url?page=$i").document
             val epsData = doc.select("#episodeLists").attr("data-content")
             if (epsData.isEmpty()) break
-            
+
             val eps = Jsoup.parse(epsData)
                 .select("a.btn.btn-sm.btn-danger")
                 .mapNotNull {
@@ -133,12 +135,12 @@ class KuramanimeProvider : MainAPI() {
 
         val typeText = document.selectFirst("div.col-lg-6.col-md-6 ul li:contains(Tipe:) a")?.text()?.lowercase() ?: "tv"
         val type = getType(typeText, episodes.size)
-        
+
         val recommendations = document.select("div#randomList > a").mapNotNull {
             val epHref = it.attr("href")
             val epTitle = it.select("h5.sidebar-title-h5.px-2.py-2").text()
             val epPoster = it.select(".product__sidebar__view__item.set-bg").attr("data-setbg")
-            if(epTitle.isNotBlank() && epHref.isNotBlank()) {
+            if (epTitle.isNotBlank() && epHref.isNotBlank()) {
                 newAnimeSearchResponse(epTitle, epHref, TvType.Anime) {
                     this.posterUrl = epPoster
                     addDubStatus(dubExist = false, subExist = true)
@@ -169,36 +171,74 @@ class KuramanimeProvider : MainAPI() {
         subtitleCallback: (SubtitleFile) -> Unit,
         callback: (ExtractorLink) -> Unit
     ): Boolean {
+        val document = app.get(data).document
 
-        val req = app.get(data)
-        val res = req.document
+        document.select("video#player > source").amap { source ->
+            safeApiCall {
+                val link = fixUrl(source.attr("src"))
+                val qualityString = source.attr("size")
+                val quality = qualityString.toIntOrNull() ?: Qualities.Unknown.value
 
-        res.select("video#player > source").forEach { source ->
-            val link = fixUrl(source.attr("src"))
-            val qualityString = source.attr("size")
-            val quality = qualityString.toIntOrNull() ?: Qualities.Unknown.value
-            
-            if (link.isNotBlank()) {
-                callback.invoke(
-                    ExtractorLink(
-                        source = "Kuramadrive",
-                        name = "Kuramadrive ${qualityString}p",
-                        url = link,
-                        referer = "$mainUrl/",
-                        quality = quality,
-                        isM3u8 = link.contains(".m3u8", true)
+                if (link.isNotBlank()) {
+                    callback.invoke(
+                        newExtractorLink(
+                            source = "Kuramadrive",
+                            name = "Kuramadrive ${qualityString}p",
+                            url = link,
+                            type = INFER_TYPE
+                        ) {
+                            this.referer = mainUrl
+                            this.quality = quality
+                        }
                     )
-                )
+                }
             }
         }
 
-        res.select("div#animeDownloadLink a").forEach {
-            val href = it.attr("href")
-            if (href.isNotBlank() && !href.contains(Regex("(?i)kdrive|kturbo|kuramadrive"))) {
-                loadExtractor(href, "$mainUrl/", subtitleCallback, callback)
+        document.select("div#animeDownloadLink a").amap { linkElement ->
+            safeApiCall {
+                val href = linkElement.attr("href")
+                val qualityText = linkElement.parent()?.previousElementSibling()?.text() ?: ""
+                val quality = getIndexQuality(qualityText)
+
+                if (href.isNotBlank() && !href.contains(Regex("(?i)kdrive|kturbo|kuramadrive"))) {
+                    loadFixedExtractor(href, quality, mainUrl, subtitleCallback, callback)
+                }
             }
         }
 
         return true
+    }
+
+    private suspend fun loadFixedExtractor(
+        url: String,
+        quality: Int?,
+        referer: String? = null,
+        subtitleCallback: (SubtitleFile) -> Unit,
+        callback: (ExtractorLink) -> Unit
+    ) {
+        loadExtractor(url, referer, subtitleCallback) { link ->
+            runBlocking {
+                callback.invoke(
+                    newExtractorLink(
+                        source = link.name,
+                        name = link.name,
+                        url = link.url,
+                        type = INFER_TYPE
+                    ) {
+                        this.referer = referer ?: mainUrl
+                        this.quality = if (link.type == ExtractorLinkType.M3U8) link.quality else quality ?: Qualities.Unknown.value
+                        this.type = link.type
+                        this.headers = link.headers
+                        this.extractorData = link.extractorData
+                    }
+                )
+            }
+        }
+    }
+
+    private fun getIndexQuality(str: String): Int {
+        return Regex("(\\d{3,4})[pP]").find(str)?.groupValues?.getOrNull(1)?.toIntOrNull()
+            ?: Qualities.Unknown.value
     }
 }
