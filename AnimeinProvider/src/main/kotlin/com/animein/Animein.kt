540872c9acb17137197646072be63598ff660a5e
HatsuneMikuUwU <146099520+HatsuneMikuUwU@users.noreply.github.com>
2026-09-24 13:26:21 +0700

    fix(animein): fallback episode image to image_cover when AniZip is empty

--- AnimeinProvider/src/main/kotlin/com/animein/Animein.kt
+++ AnimeinProvider/src/main/kotlin/com/animein/Animein.kt
@@ -369,13 +369,7 @@ class Animein : MainAPI() {
                         this.episode = epNum
                         this.score = Score.from10(metaEp?.rating)
                         this.posterUrl = metaEp?.image?.takeIf { it.isNotBlank() }
-                            ?: fixImageUrl(
-                                jStr(obj, "image", "thumbnail", "url_thumbnail", "episode_poster", "poster")
-                            )
                             ?: coverUrl
-                            ?: poster
-                            ?: backgroundPoster
-                            ?: trackerImage
                         this.description = metaEp?.overview?.takeIf { it.isNotBlank() }
                         this.addDate(metaEp?.airDateUtc)
                         this.runTime = metaEp?.runtime
